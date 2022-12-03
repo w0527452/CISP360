8086c11c344797292a335578b27908ad0cae4413
W0527452 <w0527452@apps.losrios.edu>
2022-12-03 13:59:43 -0800

    M13 assignment and start lab

---
+++ labs/M13/Mortgage.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mark Chouinard on 12/3/22.
+//
+
+#include "Mortgage.h"
---
+++ labs/M13/Mortgage.h
@@ -0,0 +1,17 @@
+//
+// Created by Mark Chouinard on 12/3/22.
+//
+
+#ifndef M13_MORTGAGE_H
+#define M13_MORTGAGE_H
+
+
+class Mortgage {
+private:
+    int years, numPayments;
+    double rate, loanAmount, payment;
+
+};
+
+
+#endif //M13_MORTGAGE_H
